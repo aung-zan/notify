46ebe5c2cf0f051841338f5560151016e404f25f
Aung Min Zan <nightkiller10@gmail.com>
2024-12-01 15:45:10 +0700

    update and modify migration files and add sidebar menu constant

---
+++ config/constant.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'sidebar' => [
+        'App' => [
+            'route' => 'app.index',
+            'icon' => '',
+        ],
+        'Email Notification' => [
+            'route' => 'email.index',
+            'icon' => '',
+        ],
+        'Push Notification' => [
+            'route' => 'push.index',
+            'icon' => '',
+        ],
+    ]
+];
